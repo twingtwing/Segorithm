9b392dc227d755812cd215c5dffff73faf4c8263
김소영 <soyoungdev.7@gmail.com>
2022-12-28 01:58:32 +0900

    221228_commit

---
+++ src/plan0/InOutPut04.java
@@ -0,0 +1,15 @@
+package plan0;
+
+public class InOutPut04 {
+    public static void main(String[] args) {
+        // 2438 :
+        // 2439 :
+        // 2440 :
+        // 2441 :
+        // 2442 :
+        // 2445 :
+        // 2446 :
+        // 2522 :
+
+    }
+}
---
+++ src/plan0/README.md
@@ -0,0 +1,13 @@
+# 입출력
+
+## 입력 객체? 함수?
+
+### Scanner, System.in
+
+nextInt() 이후에 문자를 받고 싶으면
+nextLine(); ?? 아니면 다음 문자를 가야하기 때문?
+
+### BufferedReader, InputStreamReader, System.in
+StringTokenizer
+
+![img.png](../source/sc_bf.png)
