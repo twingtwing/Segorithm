5aceae6bfa7c653d1405a6e14af2264f24df05e7
김소영 <soyoungdev.7@gmail.com>
2023-01-11 23:50:41 +0900

    220111_commit

--- src/plan2/DSdeque.java
+++ src/plan2/DSdeque.java
@@ -1,7 +1,42 @@
 package plan2;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class DSdeque {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 10866 : Deque
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(reader.readLine());
+        int [] deque = new int[count];
+        int top = -1;
+        int front = 0;
+        int rear = -1;
+
+        StringTokenizer str;
+        while (count-- > 0) {
+            str = new StringTokenizer(reader.readLine());
+            switch (str.nextToken()) {
+                case "push_front":
+                    break;
+                case "push_back":
+                    break;
+                case "pop_front":
+                    break;
+                case "pop_back":
+                    break;
+                case "size":
+                    break;
+                case "empty":
+                    break;
+                case "front":
+                    break;
+                case "back":
+                    break;
+            }
+        }
+
     }
 }
