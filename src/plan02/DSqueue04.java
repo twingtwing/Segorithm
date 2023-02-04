c06988804ef4261b153475e22d17bc28e61dc13d
김소영 <soyoungdev.7@gmail.com>
2023-02-04 16:06:40 +0900

    220204_commit

--- src/plan02/DSqueue04.java
+++ src/plan02/DSqueue04.java
@@ -1,12 +1,13 @@
 package plan02;
 
 import java.io.*;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class DSqueue04 {
     static int front;
     static int rear;
-    static int [] queue;
+    static int [] queue = new int[100];
     public static void main(String[] args) throws IOException {
         // 1966 : print 큐
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -21,24 +22,9 @@ public class DSqueue04 {
         int n;
         int len;
         int spot;
-        queue = new int[count];
 
-        while (count -- > 0){
-            front = 0;
-            rear = 0;
-            first = new StringTokenizer(reader.readLine());
-            second = new StringTokenizer(reader.readLine());
+        while (count -- > 0) {
 
-            len = Integer.parseInt(first.nextToken());
-            spot = Integer.parseInt(first.nextToken());
-
-            for (int i = 0; i < len; i++) {
-                n = Integer.parseInt(second.nextToken());
-                if (spot == i) total = totalPrint(n);
-                else total = addPrint(n);
-            }
-
-            result.append(String.valueOf(rear - front));
         }
 
         writer.write(result.toString());
@@ -46,41 +32,5 @@ public class DSqueue04 {
         reader.close();
         writer.close();
     }
-
-    //////////
-
-    private static int totalPrint(int n) {
-        int cnt = 0;
-        if (isEmpty() || queue[rear] < n) rearPush(n);
-        else if(queue[(front + 1) % queue.length] > n) frontPush(n);
-        else{
-            while (queue[rear] < n){
-                rearRemove
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-
-    private static int addPrint(int n) {
-        if (isEmpty() || queue[rear] < n) rearPush(n);
-        else if(queue[(front + 1) % queue.length] > n) frontPush(n);
-        return 0;
-    }
-
-    private static void frontPush(int n) {
-        queue[front] = n;
-        front = (queue.length + front - 1) % queue.length;
-    }
-
-    private static void rearPush(int n) {
-        rear = (rear + 1) % queue.length;
-        queue[rear] = n;
-    }
-
-    private static boolean isEmpty() {
-        return front == rear;
-    }
-
 
 }
--- src/plan02/DSstack06.java
+++ src/plan02/DSstack06.java
@@ -4,21 +4,59 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class DSstack06 {
+    static int [] stack;
+    static int [] storage;
+    static int top = -1;
+    static int stg = -1;
+    static StringBuilder result = new StringBuilder();
     public static void main(String[] args) throws IOException {
         // 2493 : 탑 레이저
+        // point!! => push시에 내림차순으로 정렬하면서 삽입하는것이 관건!!
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int count = Integer.parseInt(reader.readLine());
-        int [] stack = new int[count];
-        StringBuilder result = new StringBuilder();
+        stack = new int[count];
+        storage = new int[count];
         StringTokenizer token = new StringTokenizer(reader.readLine());
 
+        stack[++top] = Integer.parseInt(token.nextToken());
+        storage[++stg] = top;
+        result.append(String.valueOf(0)).append(" ");
+
+        while(--count  > 0){
+            stack[++top] = Integer.parseInt(token.nextToken());
+            if (stack[storage[stg]] < stack[top]){
+                storage[stg] = top;
+                if (stg > 0) recStack();
+                if (stg == 0)result.append(String.valueOf(0)).append(" ");
+                else findTop(stg - 1);
+            }else{
+                findTop(stg);
+                storage[++stg] = top;
+            }
+
+        }
 
         writer.write(result.toString());
 
         writer.close();
         reader.close();
 
+    }
+
+    private static void findTop(int start) {
+        for (int i = start; i > -1; i--){
+            if (stack[storage[i]] > stack[top]){
+                result.append(String.valueOf(storage[i] + 1)).append(" ");
+                break;
+            }
+        }
+    }
+
+    private static void recStack() {
+        if (stg < 1 || stack[storage[stg - 1]] > stack[top]) return;
+        storage[--stg] = top;
+        recStack();
     }
 }
