14c4f9edeb41a3544a51b43a6666169b3c41567f
김소영 <soyoungdev.7@gmail.com>
2023-02-21 23:22:21 +0900

    220221_commit

--- src/plan03/RecFun03.java
+++ src/plan03/RecFun03.java
@@ -1,40 +1,40 @@
 package plan03;
 
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class RecFun03 {
     public static void main(String[] args) {
         // 2447 : 별 찍기27
         Scanner scanner = new Scanner(System.in);
 
-
+        // 메모리 문제로 StringTokenizer, StringBuilder 사용을 줄임
+        // 왜 메모리가 많이 드는 걸까?
         int in = scanner.nextInt();
-        loopStars((int) Math.cbrt(in),  3);
+        int sqrt = (int) Math.pow(3,in);
 
-        scanner.close();F
+        char [][] stars = new char[sqrt][sqrt];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) stars[i][j] = ' ';
+                else stars[i][j] = '*';
+            }
+        }
+        loopStart(stars, in, 1);
+
+        scanner.close();
     }
 
-    // 메모리 문제로 StringTokenizer, StringBuilder 사용을 줄임
-    private static void loopStars(int count, int l en) {
-//        if (count == 1) return;
-//        StringTokenizer token = new StringTokenizer(result.toString(),"\n");
-//        result.setLength(0);
+    private st atic void loopStart(char[][] stars, int in, int cnt) {
+        if (cnt == in){
+            System.out.print(Arrays.toString(stars));
+            return;
+        }
+//        for (int i = 0; i < cnt*3; i++) {
 //
-//        String [] str = new String[token.countTokens()];
-//
-//        for (int i = 0; i < str.length; i++) {
-//            str[i] = token.nextToken();
-//            result.append(str[i]).append(str[i]).append(str[i]).append("\n");
 //        }
-//
-//        for (int i = 0; i < str.length; i++) {
-//            result.append(str[i]);
-//            for (int j = 0; j < len; j++) result.append(" ");
-//            result.append(str[i]).append("\n");
-//        }
-//        for (int i = 0; i < str.length; i++) result.append(str[i]).append(str[i]).append(str[i]).append("\n");
-//
-//        loopStars(result, count-1, len*3);
+        loopStart(stars,in,cnt+1);
     }
+
 }
