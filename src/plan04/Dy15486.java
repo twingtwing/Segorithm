daf3df0dcdf2d300e6263324fe43d43db446fb13
twingtwing <soyoungdev.7@gmail.com>
2023-06-13 01:06:31 +0900

    230613

---
+++ src/plan04/Dy15486.java
@@ -0,0 +1,10 @@
+package plan04;
+
+public class Dy15486 {
+    /**
+     * [15486 퇴사2]
+     **/
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/plan05/Gp1199.java
@@ -0,0 +1,10 @@
+package plan05;
+
+public class Gp1199 {
+    /**
+     * [15486 오일러 회로]
+     * */
+    public static void main(String[] args) {
+
+    }
+}
