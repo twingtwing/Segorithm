a80af98d5c3976d656091326e45924920f05cf43
김소영 <soyoungdev.7@gmail.com>
2023-07-04 15:49:17 +0900

    230704_implements?

--- src/plan09/Gd2437.java
+++ src/plan09/Gd2437.java
@@ -1,4 +1,36 @@
 package plan09;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 public class Gd2437 {
+    /**
+     * [2437 저울]
+     * */
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(reader.readLine());
+        int [] wgt = new int[N];
+        StringTokenizer tokens = new StringTokenizer(reader.readLine());
+        while (N-- > 0)
+            wgt[N] = Integer.parseInt(tokens.nextToken());
+
+        Arrays.sort(wgt);
+
+        int sum = 0;
+        for (int i = 0; i < wgt.length; i++) {
+            if (sum < wgt[i] - 1)
+                break;
+            sum += wgt[i];
+        }
+
+        System.out.println(sum + 1);
+        reader.close();
+    }
 }
---
+++ src/plan10/Ip1244.java
@@ -0,0 +1,12 @@
+package plan10;
+
+import java.util.HashSet;
+
+public class Ip1244 {
+    /**
+     * [1244 스위치 켜고 끄기]
+     * */
+    public static void main(String[] args) {
+        HashSet<String> [] sets =  new HashSet[1];
+    }
+}
