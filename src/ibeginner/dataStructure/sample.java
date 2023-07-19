e073157041b93f6fef889022c2daf06b0280d906
김소영 <soyoungdev.7@gmail.com>
2023-07-19 18:04:04 +0900

    0719_Stack

--- src/ibeginner/dataStructure/sample.java
+++ src/ibeginner/dataStructure/sample.java
@@ -1,4 +1,10 @@
 package ibeginner.dataStructure;
 
 public class sample {
+    public static void main(String[] args) {
+        // 2504 괄호 제거
+        // 9935
+        // 22942
+        // 17299
+    }
 }
---
+++ src/ibeginner/dataStructure/stack/St2504.java
@@ -0,0 +1,15 @@
+package ibeginner.dataStructure.stack;
+
+import java.util.Scanner;
+
+public class St2504 {
+    /**
+     * [2504 괄호의 값]
+     * */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char [] chars = sc.nextLine().toCharArray();
+
+        sc.close();
+    }
+}
