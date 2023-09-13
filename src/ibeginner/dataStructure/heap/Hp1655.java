4ec717919dc00bd2077b235797d996e018c895fe
김소영 <soyoungdev.7@gmail.com>
2023-09-13 18:27:34 +0900

    오늘은 heap end 내일 deque, set, map end

--- src/ibeginner/dataStructure/heap/Hp1655.java
+++ src/ibeginner/dataStructure/heap/Hp1655.java
@@ -3,8 +3,6 @@ package ibeginner.dataStructure.heap;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
 
 public class Hp1655 {
     /**
@@ -15,21 +13,28 @@ public class Hp1655 {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(reader.readLine());
 
-        PriorityQueue<Integer> asc = new PriorityQueue<>();
-        PriorityQueue<Integer> desc = new PriorityQueue<>(Collections.reverseOrder());
-        asc.offer(Integer.parseInt(reader.readLine()));
-
+        MaxHeap maxHeap = new MaxHeap(N);
+        MinHeap minHeap = new MinHeap(N);
         StringBuilder result = new StringBuilder();
-        result.append(asc.peek()).append("\n");
-        for (int i = 1; i < N; i++){
+
+        while (N-->0){
             int n = Integer.parseInt(reader.readLine());
-            if (i % 2 == 0){
 
-            }else{
-                if (asc.peek() > n){
+            // 원소 주입
+            // 중간 값을 효율적으로 찾기 위해서
+            // maxHeap < n < minHeap 은 minHeap에 넣는다.
+            if (maxHeap.isEmpty() || n <= maxHeap.peek())
+                maxHeap.offer(n);
+            else
+                minHeap.offer(n);
 
-                }
-            }
+            // 크기 조절
+            if (maxHeap.size() > minHeap.size() + 1)
+                minHeap.offer(maxHeap.poll());
+            else if (minHeap.size() > maxHeap.size())
+                maxHeap.offer(minHeap.poll());
+
+            result.append(maxHeap.peek()).append("\n");
         }
 
         System.out.println(result);
@@ -37,3 +42,114 @@ public class Hp1655 {
     }
 }
 
+class MinHeap{
+    int size;
+    int [] heap;
+
+    public MinHeap(int len){
+        this.size = 0;
+        this.heap = new int[len + 1];
+    }
+
+    public boolean isEmpty(){
+        return this.size == 0;
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    public void offer(int data){
+        heap[++size] = data;
+
+        int child = size;
+        while (child > 1 && heap[child] < heap[child/2]){
+            swap(child, child/2);
+            child/=2;
+        }
+
+    }
+
+    public int poll(){
+        int min = heap[1];
+        heap[1] = heap[size--];
+
+        int parent = 1;
+        while (parent * 2 <= size){
+            int child = parent * 2;
+            if (child < size && heap[child] > heap[child + 1])
+                child++;
+            if (heap[parent] <= heap[child])
+                break;
+            swap(parent, child);
+            parent = child;
+        }
+
+        return min;
+    }
+
+    private void swap(int i,  int j){
+        int tmp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = tmp;
+    }
+}
+
+class MaxHeap{
+    int size;
+    int [] heap;
+
+    public MaxHeap(int len){
+        this.size = 0;
+        this.heap = new int[len + 1];
+    }
+
+    public boolean isEmpty(){
+        return this.size == 0;
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    // 상향식
+    public void offer(int data){
+        heap[++size] = data;
+
+        int child = size;
+        while (child > 1 && heap[child] > heap[child / 2]){
+            swap(child, child/2);
+            child /= 2;
+        }
+    }
+
+    // 하향식
+    public int poll(){
+        int max = heap[1];
+        heap[1] = heap[size--];
+
+        int parent = 1;
+        while (parent * 2 <= size){
+            int child = parent * 2;
+            if (child < size && heap[child] < heap[child + 1])
+                child++;
+            if (heap[parent] >= heap[child])
+                break;
+            swap(parent, child);
+            parent = child;
+        }
+
+        return max;
+    }
+
+    public int peek(){
+        return heap[1];
+    }
+
+    private void swap(int i,  int j){
+        int tmp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = tmp;
+    }
+
+}
---
+++ src/ibeginner/dataStructure/heap/Hp2696.java
@@ -0,0 +1,112 @@
+package ibeginner.dataStructure.heap;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Hp2696 {
+    /**
+     * [2696 중앙값 구하기]
+     * */
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder result = new StringBuilder();
+        int N = Integer.parseInt(reader.readLine());
+
+        while (N-->0){
+            int len = Integer.parseInt(reader.readLine());
+            StringTokenizer str = new StringTokenizer(reader.readLine());
+            Heap maxHeap = new Heap(len, Type.MAX);
+            Heap minHeap = new Heap(len, Type.MIN);
+            for (int i = 0; i < len; i++) {
+                if (len % 10 == 0)
+                    str = new StringTokenizer(reader.readLine());
+                int n = Integer.parseInt(str.nextToken());
+                if (maxHeap.isEmpty() || maxHeap.peek() >= n)
+                    maxHeap.offer(n);
+                else
+                    minHeap.offer(n);
+
+                if (maxHeap.getSize() > minHeap.getSize() + 1)
+                    minHeap.offer(maxHeap.poll());
+                else if(minHeap.getSize() > maxHeap.getSize())
+                    maxHeap.offer(minHeap.poll());
+
+                if (i % 2 == 1)
+                    result.append(maxHeap.peek()).append(" ");
+            }
+            result.append("\n");
+        }
+
+        System.out.print(result);
+        reader.close();
+    }
+}
+
+enum Type{
+    MAX, MIN
+}
+
+class Heap{
+    private int size;
+    private Type type;
+    private int [] heap;
+
+    public Heap(int len, Type type){
+        this.size = 0;
+        this.type = type;
+        this.heap = new int[len + 1];
+    }
+
+    public int getSize(){
+        return this.size;
+    }
+
+    public boolean isEmpty(){
+        return this.size == 0;
+    }
+
+    public void offer(int data){
+        heap[++size] = data;
+
+        int child = size;
+        while (child > 1 &&
+                this.type == Type.MAX ?
+                heap[child] > heap[child + 1] : heap[child] < heap[child + 1]){
+            swap(child, child/2);
+            child/=2;
+        }
+    }
+
+    public int poll(){
+        int root = heap[1];
+        heap[1] = heap[size--];
+
+        int parent = 1;
+        while (parent * 2 <= size){
+            int child = parent * 2;
+            if (child < size &&
+                    this.type == Type.MAX ?
+                    heap[child] < heap[child + 1] : heap[child] > heap[child + 1])
+                child++;
+            if (this.type == Type.MAX ? heap[parent] >= heap[child] : heap[parent] <= heap[child])
+                break;
+            swap(parent, child);
+            parent = child;
+        }
+
+        return root;
+    }
+
+    public int peek(){
+        return heap[1];
+    }
+
+    private void swap(int i, int j) {
+        int tmp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = tmp;
+    }
+}
