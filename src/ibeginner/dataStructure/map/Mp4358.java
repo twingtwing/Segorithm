f53dbd471ec2227abbf7121fdbc9159bc94b543d
qazxc753 <qazxc753@computermate.co.kr>
2023-09-21 18:13:24 +0900

    END 주말에

---
+++ src/ibeginner/dataStructure/map/Mp4358.java
@@ -0,0 +1,17 @@
+package ibeginner.dataStructure.map;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Mp4358 {
+    /**
+     * [4195 생태학]
+     * */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        reader.close();
+    }
+}
